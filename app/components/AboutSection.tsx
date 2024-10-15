3490f346902858f5774d3db0438d4fac2e2d4115
Zora092 <brookesalazar2005@gmail.com>
2024-10-15 12:16:14 -0500

    Modified About Section

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -8,22 +8,12 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2 grid grid-cols-4 gap-4">
-        <li>Node.js</li>
-        <li>Next.js</li>
-        <li>PostgreSQL</li>
-        <li>Unity</li>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Godot</li>
-        <li>C#</li>
-        <li>Python</li>
-        <li>C++</li>
-        <li>OpenCV</li>
-        <li>Git</li>
-        <li>Google Cloud Platform</li>
-        <li>LLM</li>
-        <li>Deep Learning</li>
-        <li>VR/AR</li>
+        <li>Baking</li>
+        <li>Gaming</li>
+        <li>drawing</li>
+        <li>Working with animals</li>
+        <li>Cooking</li>
+        
       </ul>
       
     ),
@@ -33,8 +23,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Master&apos;s Computer Science - University of Texas Rio Grande Valley</li>
-        <li>Bachelor&apos;s Computer Science - University of Texas Rio Grande Valley</li>
+      <li>High School degree&apos;s Lasara ISD</li>
+        <li>Associate's degree&apos; Science - Costal Bend College </li>
       </ul>
     ),
   },
@@ -66,13 +56,11 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a Game Developer with interest of many topics, from Machine Learning, to Theory.
-            Different skills I have acquired over the years to consider myself a Jack of All Trades, and
-            master to game development since its quite fun.
+            I am a student at UTRGV and the career path i am going into is computer science. 
+            I am planning to go into game development at Full Sail University once I get my degree here. 
 
-            My enthusiasm for technology drives me to continue to learn something new, and I have enjoyed
-            learning different topics and also teaching others to pursue certain topics if it finds their
-            intereset. 
+            My enthusiasm for this career has come from my childhood. I have always
+             loved to play games but I wanted to know more.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
